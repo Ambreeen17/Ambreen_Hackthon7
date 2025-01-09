7b80f29efa7096c1038852a1a733b5ec3cba0c36
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2025-01-09 09:41:48 +0500

    Update Header.tsx

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,17 +2,9 @@
 import React, { useState } from "react"
 import Image from "next/image";
 import Link from "next/link";
-import { FaSearch,  FaHeart, FaBell, FaCog, FaTimes, FaBars } from "react-icons/fa";
-import { FaHome } from 'react-icons/fa'; // FontAwesome
+import { FaSearch, FaHome, FaHeart, FaBell, FaCog, FaTimes, FaBars } from "react-icons/fa";
 import { VscSettings } from "react-icons/vsc";
 
-
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 87d2f9d1b45e4cf362e9f664359fce03bfdbab4d
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
